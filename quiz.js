99dcb97b8a9ab84fa5651cd2f53be1b307d23fbf
vvbs <131943481+vvbs@users.noreply.github.com>
2023-06-08 15:00:48 +0900

    quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,50 @@
+var questions = [
+  {
+    question: "日本の首都はどこですか？",
+    correct_answer: "東京"
+  },
+  {
+    question: "「こんにちは」は英語で何と言いますか？",
+    correct_answer: "Hello"
+  },
+  {
+    question: "1+1はいくつですか？",
+    correct_answer: "2"
+  }
+];
 
+var currentQuestion = 0;
+var score = 0;
+
+function displayQuestion() {
+  var questionDiv = document.getElementById("question");
+  questionDiv.innerHTML = questions[currentQuestion].question;
+}
+
+function checkAnswer() {
+  var answerInput = document.getElementById("answer");
+  var userAnswer = answerInput.value;
+
+  if (userAnswer.toLowerCase() === questions[currentQuestion].correct_answer.toLowerCase()) {
+    score++;
+    document.getElementById("result").innerHTML = "正解です！";
+  } else {
+    document.getElementById("result").innerHTML = "不正解です。";
+  }
+
+  currentQuestion++;
+
+  if (currentQuestion < questions.length) {
+    displayQuestion();
+    answerInput.value = "";
+  } else {
+    displayScore();
+  }
+}
+
+function displayScore() {
+  var scoreDiv = document.getElementById("result");
+  scoreDiv.innerHTML = "クイズ終了！<br>あなたのスコアは " + score + " 点です。";
+}
+
+displayQuestion();
